d848c5dd69b67e118a2c3f72bce1f564f1ec0751
canberk <canberkozler0@gmail.com>
2023-05-07 23:06:02 +0300

    add elegant way to calculate fibonacci.

--- Fibonacci/Fibonacci.cpp
+++ Fibonacci/Fibonacci.cpp
@@ -1,9 +1,21 @@
 #include<iostream>
 #include<utility>
 
-// https://en.cppreference.com/w/cpp/utility/exchange
+// Elegant Way
+template<size_t n>
+struct Fibonacci {
+	static const size_t value{ n > 1 ? n + Fibonacci<n - 1>::value : 1};
+};
+
+template<>
+struct Fibonacci<0> {
+	static const size_t value{ 0 };
+};
 
 int main(){
+	std::cout << Fibonacci<10>::value << "\n";
+
+    // https://en.cppreference.com/w/cpp/utility/exchange
     size_t N{};
     std::cout << "Enter the upper bound?\n";
     std::cin >> N;
