92385a9ccb0772647dcf1f9a2f0253f51359f2ff
canberk <canberkozler0@gmail.com>
2023-08-13 17:46:40 +0300

    Fix: Line Alignment.

--- SearchAlgorithms/BellmanFord/BellmanFord.cpp
+++ SearchAlgorithms/BellmanFord/BellmanFord.cpp
@@ -15,37 +15,37 @@ public:
     }
 
     void bellmanFord(int src){
-    std::vector<int> dist(N, INF); // Initialize all distances to infinity.
-    dist[src] = 0; // Distance to the source vertex is 0.
+        std::vector<int> dist(N, INF); // Initialize all distances to infinity.
+        dist[src] = 0; // Distance to the source vertex is 0.
 
-    // Relax all edges N-1 times
-    for (int i = 1; i <= N - 1; ++i) {
+        // Relax all edges N-1 times
+        for (int i = 1; i <= N - 1; ++i) {
+            for (const auto& edge : edges) {
+                int u = edge.second.first;
+                int v = edge.second.second;
+                int w = edge.first;
+                if (dist[u] != INF && (dist[u] + w) < dist[v]) {
+                    dist[v] = dist[u] + w; // Edge relaxation
+                }
+            }
+        }
+
+        // Check for negative cycles
         for (const auto& edge : edges) {
             int u = edge.second.first;
             int v = edge.second.second;
             int w = edge.first;
             if (dist[u] != INF && (dist[u] + w) < dist[v]) {
-                dist[v] = dist[u] + w; // Edge relaxation
+                hasNegativeCycle = true;
             }
         }
-    }
 
-    // Check for negative cycles
-    for (const auto& edge : edges) {
-        int u = edge.second.first;
-        int v = edge.second.second;
-        int w = edge.first;
-        if (dist[u] != INF && (dist[u] + w) < dist[v]) {
-            hasNegativeCycle = true;
+        if (!hasNegativeCycle) {
+            // Print all distances from the origin to all vertices.
+            for (int i = 0; i < N; ++i) {
+                std::cout << i << " - " << dist[i] << '\n';
+            }
         }
-    }
-
-    if (!hasNegativeCycle) {
-        // Print all distances from the origin to all vertices.
-        for (int i = 0; i < N; ++i) {
-            std::cout << i << " - " << dist[i] << '\n';
-        }
-    }
     }
 
 private:
