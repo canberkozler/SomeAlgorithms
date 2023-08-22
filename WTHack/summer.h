a8c190f5042960e37dc6e76f706ea52d727b1a8c
canberk <canberkozler0@gmail.com>
2023-08-22 19:56:22 +0300

    New function for summing is added.

--- WTHack/summer.h
+++ WTHack/summer.h
@@ -7,3 +7,12 @@ inline auto sum(Args... args) // GOOD, and much more flexible
 {
     return (... + args); // note: C++17 "fold expression"
 }
+
+template<typename T>
+inline T summer(T u){
+    return u;
+}
+template<typename T, typename ...Args>
+inline T summer(const T& fs, Args... args){
+    return fs + summer(args...);
+}
