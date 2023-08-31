0458101d2640be0517a667a006755042f72c78b5
canberk <canberkozler0@gmail.com>
2023-08-31 21:12:44 +0300

    Add printers while using STL algorithm.

--- Print/print.cpp
+++ Print/print.cpp
@@ -81,9 +81,32 @@ int main(){
 
         88.9_print;
     }
+
     std::cout << "Apply double new line..."<<double_new_line;
     {
         std::vector<int> ivec{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; 
-        println("The vector contains :", ivec);
+        println("The vector contains: ", ivec);
+    }
+
+    { // Using STL algorithm to print.
+        std::vector<int> ivec{34, 2, 10, -9, 22, 356, 124, 89, -98, -23, 56};
+
+        std::ostream_iterator<int> outputIter{std::cout, " "};
+        std::cout << "Using std::copy to print: ";
+        std::copy(ivec.begin(), ivec.end(), outputIter);
+        std::cout <<'\n';
+
+        std::cout << "Using std::transform to print: ";
+        std::transform(ivec.begin(), ivec.end(), outputIter, [](auto elem)
+                       { return elem; });
+        std::cout <<'\n';
+
+        std::cout << "Using std::generate_n to print: ";
+        std::generate_n(outputIter, ivec.size(),
+                        [&ivec](){
+                            static size_t index = 0;
+                            return ivec[index++];
+                        });
+        std::cout <<'\n';
     }
 }
