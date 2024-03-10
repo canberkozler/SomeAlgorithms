22e7a399e4269f78349fb146708ac19db8e26280
canberk <canberkozler0@gmail.com>
2024-03-10 20:37:24 +0300

    Add ExtendedGCD.

---
+++ GCD/ExtendedGCD/extended_gcd.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int gcdExtended(int a, int b, int& x, int& y){
+    if (a == 0) {
+        x = 0;
+        y = 1;
+        return b;
+    }
+
+    int x1, y1;
+    int gcd = gcdExtended(b % a, a, x1, y1);
+
+    x = y1 - (b / a) * x1;
+    y = x1;
+
+    return gcd;
+}
+
+int main()
+{
+    int x, y, a = 60, b = 15;
+    int g = gcdExtended(a, b, x, y);
+
+    std::cout << "gcd(" << a << ", " << b << ") = " << g << "\n";
+}
